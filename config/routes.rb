565703b69e4445167dc45f3ab8e974b8cecdc806
Mohammed Noori <kmanoorib@gmail.com>
2019-05-23 01:42:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
+
+
+
+# Rails.application.routes.draw do
+#   resources :brands, only: [:index, :show] do
+#     resources :products, only: [:index, :show]
+#   end
+#  
+#   resource :basket, only: [:show, :update, :destroy]
+#  
+#   resolve("Basket") { route_for(:basket) }
+# end
